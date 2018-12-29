7f80289b371969610d94c94b3a54597eef62f558
GeorgyBelski <45123704+GeorgyBelski@users.noreply.github.com>
2018-12-30 00:10:28 +0100

    Add Zombie Animation

---
+++ Assets/ZombieAnimationController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZombieAnimationController : MonoBehaviour
+{
+    public float animationDamping = 0.15f;
+
+    Animator animator;
+    Vector3 velocity;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        velocity = transform.position;
+    }
+
+    void FixedUpdate()
+    {
+        bool move = (velocity - transform.position).magnitude > 0;
+        animator.SetBool("move", move);
+        velocity = transform.position;
+    }
+}
