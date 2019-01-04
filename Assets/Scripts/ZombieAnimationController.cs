599d2261549923a72b0960721aa7c48106ca04cb
GeorgyBelski <45123704+GeorgyBelski@users.noreply.github.com>
2019-01-04 17:35:57 +0100

    Correcting Zombie Animation

---
+++ Assets/Scripts/ZombieAnimationController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditorInternal;
+using UnityEngine;
+
+public class ZombieAnimationController : MonoBehaviour
+{
+    public float zSpeed;
+
+
+
+    Animator animator;
+    Vector3 velocity;
+    Vector3 rootPosition;
+    Zombie_script1 zScript;
+    
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        animator.SetFloat("speedMultiplier", Random.RandomRange(1f,2f));
+        velocity = transform.position;
+        zScript = GetComponent<Zombie_script1>();
+    }
+
+    void FixedUpdate()
+    {
+        
+      //  Vector3 rootPosition = animator.rootPosition;
+        //   position.y = agent.nextPosition.y;
+    }
+
+    void OnAnimatorMove()
+    {
+        zSpeed = zScript.speed;
+        bool move = zScript.speed > 0.01f;
+        animator.SetBool("move", move);
+        // Update position based on animation movement using navigation surface height
+        rootPosition = animator.rootPosition;
+     //   position.y = agent.nextPosition.y;
+        transform.position = rootPosition;
+    }
+}
--- Assets/Scripts/Zombie_script1.cs
+++ Assets/Scripts/Zombie_script1.cs
@@ -1,80 +1,149 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class Zombie_script1 : MonoBehaviour {
+public class Zombie_script1 : MonoBehaviour
+{
     public GameObject enemy;
     private GameObject player;
+    public Image helthBar;
     private float startSpeed;
     //private Rigidbody rg;
     public float speed = 10f;
-	// Use this for initialization
-	void Start () {
+    public bool attack = false;
+    // Use this for initialization
+    void Start()
+    {
         player = GameObject.Find("Player");
         //Transform tr = GetComponent<enemy.>
         startSpeed = speed;
+        speed = 0f;
         //rg = GetComponent<Rigidbody>();
 
     }
-	
-	// Update is called once per frame
-	void Update () {
-        
-		
-	}
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+    }
     //private float x_displacement, y_displacement;
 
     void FixedUpdate()
     {
-        if (enemy) { 
-            transform.LookAt(new Vector3( enemy.transform.position.x, transform.position.y, enemy.transform.position.z));
-            transform.position = Vector3.MoveTowards(new Vector3(transform.position.x, transform.position.y, transform.position.z) , new Vector3(enemy.transform.position.x, transform.position.y, enemy.transform.position.z) , speed/100);
+        if (enemy)
+        {
+            transform.LookAt(new Vector3(enemy.transform.position.x, transform.position.y, enemy.transform.position.z));
+        //    transform.position = Vector3.MoveTowards(new Vector3(transform.position.x, transform.position.y, transform.position.z), new Vector3(enemy.transform.position.x, transform.position.y, enemy.transform.position.z), speed / 100);
             //transform.position = new Vector3
         }
     }
 
     void OnCollisionEnter(Collision collision)
     {
-        if(collision.gameObject.CompareTag("Player"))
+        if (collision.gameObject.CompareTag("Player"))
         {
+         //   Debug.Log("OnCollisionEnter");
             speed = 0;
+            attack = true;
+        }
+        if (collision.gameObject.CompareTag("Bullet"))
+        {
+            helthBar.fillAmount -= 0.2f;
+            if (helthBar.fillAmount <= 0.01)
+            {
+                Destroy(gameObject);
+            }
+
         }
         //speed = 0;
         //collision.gameObject.CompareTag("bullet_Tag")
     }
     void OnCollisionExit(Collision collision)
     {
-        
+
         //if(collision.gameObject.CompareTag("Player")){
-            speed = startSpeed;
+        attack = false;
         //}
     }
-    void OnTriggerEnter(Collider other)
+    void OnTriggerStay(Collider other)
     {
-        //print("+");
-        if (other.gameObject.CompareTag("Player"))
+        if(!attack)
+            speed = startSpeed;
+        if (other.gameObject.CompareTag("Human"))
         {
-            
-            enemy = player;
+            enemy = other.gameObject;
+
+        }
+        else
+        {
+            if (!enemy)
+                enemy = other.gameObject;
+        }
+
+
+
+
+    }
+
+    /*void OnTriggerStay(Collider other)
+    {
+        print("+");
+        if (other.gameObject.CompareTag("Player") || other.gameObject.CompareTag("Human"))
+        {
+            if (other.gameObject.CompareTag("Human")) {
+                enemy = GameObject.Find("Human");
+            }
+            else
+            {
+                enemy = player;
+            }
+
             //Zombie_script zomb = collision.gameObject;//GetComponent<collision.gameObject>();
             //collision.fi
             //speed = 0;
         }
+
         //speed = 0;
         //collision.gameObject.CompareTag("bullet_Tag")
     }
-    void OnTriggerExit(Collider other)
-    {
-        // print("+");
-        //if (other.gameObject.CompareTag("Player"))
-        //{
+    */
 
-            enemy = null;
+    /*
+    void OnTriggerEnter(Collider other)
+    {
+print("+");
+        if (other.gameObject.CompareTag("Player") || other.gameObject.CompareTag("Human"))
+        {
+            if (other.gameObject.CompareTag("Human"))
+            {
+                enemy = GameObject.Find("Human");
+            }
+            else
+            {
+                enemy = player;
+            }
+
             //Zombie_script zomb = collision.gameObject;//GetComponent<collision.gameObject>();
             //collision.fi
             //speed = 0;
-        //}
+        }
+    }
+    */
+    void OnTriggerExit(Collider other)
+    {
+       // print("+");
+        //if (other.gameObject.CompareTag("Player"))
+        //{
+
+        enemy = null;
+        //Zombie_script zomb = collision.gameObject;//GetComponent<collision.gameObject>();
+        //collision.fi
         //speed = 0;
+        //}
+        speed = 0;
         //collision.gameObject.CompareTag("bullet_Tag")
     }
 }
