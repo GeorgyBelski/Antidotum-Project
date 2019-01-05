763dc4bdfa7275d982f49162c736e142ddfc39d9
GeorgyBelski <45123704+GeorgyBelski@users.noreply.github.com>
2019-01-05 22:07:42 +0100

    Add Collecting Biomaterials

--- Assets/Scripts/PlayerAttributes.cs
+++ Assets/Scripts/PlayerAttributes.cs
@@ -11,15 +11,25 @@ public class PlayerAttributes : MonoBehaviour
     [Range(0, 200)]
     public int health;
 
+    [Space]
+
+    [Range(10, 30)]
+    public int maxBioAmount = 10;
+    public int bioAmount = 0;
+    public int anidoreAmount = 0;
+
     Image currentHealsBar;
+    Image currentBioBar;
     SphereCollider detectionSphere;
     int previousHealth;
+    int previousBioAmount = -1;
 
     void Start()
     {
         health = maxHealth;
         previousHealth = health;
         currentHealsBar = gameObject.transform.Find("Canvas_Health/Image_Health_Bar").GetComponent<Image>();
+        currentBioBar = gameObject.transform.Find("Canvas_Bio/Image_Bio_Bar").GetComponent<Image>();
         detectionSphere = GetComponent<SphereCollider>();
         detectionSphere.radius = health + 20;
         currentHealsBar.rectTransform.localScale = new Vector3(1, 1, 1);
@@ -28,21 +38,28 @@ public class PlayerAttributes : MonoBehaviour
     void Update()
     {
         if(health != previousHealth) { 
-            float retio = (float) health / maxHealth;
-            currentHealsBar.rectTransform.localScale = new Vector3(retio,1,1);
+            float retioHealth = (float) health / maxHealth;
+            currentHealsBar.rectTransform.localScale = new Vector3(retioHealth, 1,1);
             detectionSphere.radius = health + 20;
+        }
+        if (bioAmount != previousBioAmount) {
+            float retioBio = (float)bioAmount / maxBioAmount;
+            currentBioBar.rectTransform.localScale = new Vector3(1, retioBio, 1);
         }
     }
 
     void OnCollisionEnter(Collision collision)
     {
-        print("DNA");
+        //print("DNA");
         if (collision.gameObject.CompareTag("DNA"))
         {
 
             //dna.text = "1";
             Destroy(collision.gameObject);
-            //Destroy(gameObject);
+            
+            if (bioAmount < maxBioAmount) { 
+                bioAmount++;
+            }
         }
     }
 
